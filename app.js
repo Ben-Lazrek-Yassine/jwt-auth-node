943486ca2993126d8830bd4db7c4a73f9373c674
Ben-Lazrek-Yassine <dogdevil181@gmail.com>
2023-07-01 11:30:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://admin:test123@cluster0.qcsdojb.mongodb.net/node-auth';
+const dbURI = 'xxx';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
