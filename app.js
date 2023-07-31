7f2268bcbf1f8814b4f7eabb0ccf562e01ce7f08
Ben-Lazrek-Yassine <dogdevil181@gmail.com>
2023-07-31 14:39:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
-const { requireAuth } = require('./middleware/authMiddle');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddle');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 app.set('view engine', 'ejs');
-
-const dbURI = 'xxxx';
+const dbURI = 'xxx';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.get('/test', requireAuth, (req, res) => res.render('test'));
 app.use(authRoutes);
-
